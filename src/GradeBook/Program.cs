243c8b668884a6b7535aa3953216358eec8a08b6
cbelgr4ve <cbelgr4ve@gmail.com>
2019-08-31 21:51:54 -0400

    Test Commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            double x = 34.1;
+            double y = 66.6;
+            double average = (x + y) / 2;
+
+            if(args.Length > 0)
+            {  
+            Console.WriteLine($"Hello, {args[0]}! Your average is {average}%!");
+            } 
+            else {
+            Console.WriteLine("Hello!");
+            }   
+        }
+    }
+}
